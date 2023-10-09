5f7c1dc1b954fae186054e2ab59db5ccdb006d3d
Jamsh1dOzodov <o.jamshidbek12@gmail.com>
2023-10-09 16:38:57 +0500

    added home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,22 @@
+import home_logo from '../assets/icons/home_logo.svg'
 
 
 const Home = () => {
     return (
         <>
-        
+            <div className="home">
+                <div className="container">
+                    <div className='home-header'>
+                        <img className='home-header__logo' src={home_logo} />
+                        <h1 className="home-header__title">Book Reader</h1>
+                    </div>
+                    <h4 className='home__text'>Читай и слушай где угодно вместе с Book Reader</h4>
+                    <div className='home-regBtns'>
+                        <button className='home-regBtns__btn home-regBtns__btn--reg'>Зарегистрироваться</button>
+                        <button className='home-regBtns__btn home-regBtns__btn--sign'>Войти</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
